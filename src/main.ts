03b70afb406d7851e83121d143d0f8f2669c23a7
brian-o <brianodellk@gmail.com>
2020-07-31 22:35:31 -0700

    made code compliant with linter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { Builder } from "roles/Builder";
 import { ErrorMapper } from "utils/ErrorMapper";
 import { Harvester } from "roles/Harvester";
-import { Builder } from "roles/Builder";
 import { Upgrader } from "roles/Upgrader";
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
@@ -16,68 +16,72 @@ export const loop = ErrorMapper.wrapLoop(() => {
   }
 
   // spawn time bby
-  let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-  console.log('Harvesters: ' + harvesters.length);
+  const harvesters = _.filter(Game.creeps, creep => creep.memory.role === "harvester");
+  console.log(`Upgraders: ${harvesters.length}`);
 
   // should probably have a check on if there is enough energy xD
   if (harvesters.length < 2) {
-      let newName = 'Harvester' + Game.time;
-      console.log('Spawning new harvester: ' + newName);
-      Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
-          {memory: {
-            role: 'harvester',
-            room: '',
-            working: true
-          }});
+    const newName = `Harvester ${Game.time}`;
+    console.log("Spawning new harvester: " + newName);
+    Game.spawns.Spawn1.spawnCreep([WORK, CARRY, MOVE], newName, {
+      memory: {
+        role: "harvester",
+        room: "",
+        working: true
+      }
+    });
   }
 
-  let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+  const upgraders = _.filter(Game.creeps, creep => creep.memory.role === "upgrader");
   console.log(`Upgraders: ${upgraders.length}`);
 
   if (upgraders.length < 1) {
-    let newName = `Upgrader${Game.time}`;
-    console.log(`Spawning new upgrader: ${newName}`)
-    Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
-        {memory: {
-          role: 'upgrader',
-          room: '',
-          working: true
-        }});
+    const newName = `Upgrader${Game.time}`;
+    console.log(`Spawning new upgrader: ${newName}`);
+    Game.spawns.Spawn1.spawnCreep([WORK, CARRY, MOVE], newName, {
+      memory: {
+        role: "upgrader",
+        room: "",
+        working: true
+      }
+    });
   }
 
-  let builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-  console.log(`Builders: ${builders.length}`)
+  const builders = _.filter(Game.creeps, creep => creep.memory.role === "builder");
+  console.log(`Builders: ${builders.length}`);
 
   if (builders.length < 1) {
-    let newName = `Builder${Game.time}`;
-    console.log(`Spawning new builder: ${newName}`)
-    Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
-        {memory: {
-          role: 'builder',
-          room: '',
-          working: true
-        }});
+    const newName = `Builder${Game.time}`;
+    console.log(`Spawning new builder: ${newName}`);
+    Game.spawns.Spawn1.spawnCreep([WORK, CARRY, MOVE], newName, {
+      memory: {
+        role: "builder",
+        room: "",
+        working: true
+      }
+    });
   }
 
-  if (Game.spawns['Spawn1'].spawning) {
-      let spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-      Game.spawns['Spawn1'].room.visual.text(
-          '🛠️' + spawningCreep.memory.role,
-          Game.spawns['Spawn1'].pos.x + 1,
-          Game.spawns['Spawn1'].pos.y,
-          {align: 'left', opacity: 0.8});
+  if (Game.spawns.Spawn1.spawning) {
+    const spawningCreep = Game.creeps[Game.spawns.Spawn1.spawning.name];
+    Game.spawns.Spawn1.room.visual.text(
+      "🛠️" + spawningCreep.memory.role,
+      Game.spawns.Spawn1.pos.x + 1,
+      Game.spawns.Spawn1.pos.y,
+      { align: "left", opacity: 0.8 }
+    );
   }
 
-  for (let name in Game.creeps) {
-    let creep = Game.creeps[name];
-    if (creep.memory.role == 'harvester') {
-        Harvester.run(creep);
+  for (const name in Game.creeps) {
+    const creep = Game.creeps[name];
+    if (creep.memory.role === "harvester") {
+      Harvester.run(creep);
     }
-    if (creep.memory.role == 'upgrader') {
-        Upgrader.run(creep);
+    if (creep.memory.role === "upgrader") {
+      Upgrader.run(creep);
     }
-    if (creep.memory.role == 'builder') {
-        Builder.run(creep);
+    if (creep.memory.role === "builder") {
+      Builder.run(creep);
     }
   }
 });
